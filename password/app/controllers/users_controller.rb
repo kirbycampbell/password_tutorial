46250c3e3167f3db84be347c12e6c30c57f00d8e
Kirby Campbell <jkirbycampbell@gmail.com>
2018-10-04 13:10:54 -0700

    Finished Tutorial

--- password/app/controllers/users_controller.rb
+++ password/app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 class UsersController < ApplicationController
 
   def create
-    User.create(user_params)
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path
+    end
+  end
+
+  def show
+    @user = User.find_by(params[:user_id])
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :password_field, :password_confirmation)
+    params.require(:user).permit(:username, :password, :password_confirmation)
 
   end
 
