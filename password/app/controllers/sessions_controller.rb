ad49900b1a2bbd014c9919aff8e51014150a98fb
Kirby Campbell <jkirbycampbell@gmail.com>
2018-10-04 12:50:47 -0700

    Adding to File

--- password/app/controllers/sessions_controller.rb
+++ password/app/controllers/sessions_controller.rb
@@ -1,2 +1,9 @@
 class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by(username: params[:username])
+    return head(:forbidden) unless @user.authenticate(params[:password_field])
+    session[:user_id] = @user.id
+  end
+
 end
--- password/app/controllers/users_controller.rb
+++ password/app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+  def create
+    User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password_field, :password_confirmation)
+
+  end
+
 end
